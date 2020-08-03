4193600cc73224b5d810c7e144da7ddd36c39bdf
Saimon <56609203+ribeirosaimon@users.noreply.github.com>
2020-08-03 20:43:07 -0300

    comit

--- teste.py
+++ teste.py
@@ -3,7 +3,14 @@ from scraping.date_tratament import date_treatment, dataIso
 from scraping.scraping_stock import tratamento_acao
 
 
+with open('acoes_ibov.csv','r') as acoes:
+    for x in acoes:
+        print(x)
+
+'''
+
 stock = 'vvar3'
 acao = soup_url(stock)
 ultima_deteccao = ultimo_topo_e_fundo_da_acao(acao)
 print(ultima_deteccao)
+'''
